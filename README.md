8e7e7b39d5ccc9c9eee7ee01b4745b6932ac0089
fernando_R <fer0624@gmail.com>
2018-07-17 23:43:47 -0700

    final update.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RPG.Game
+#This yugi oh game allows you to select a chatacter and will ramdomly select an enemy that you will confront.
+# as you click on Arrack Button cardspower for you and your enemy will randomly change and whover gets to have the best cardset throught the game will win.
+# on every attack if cards power is less than enemy you will loose one health point else opponent will loose one.
+#also you might fight agains yourslef rememeber in the anime they have evil twings from 2000 years ago i know i can use include to avoid this but im trying to be close to the script.
+# you win if Enemy heatl is less than 0.
+# This game follows 95% of the Instructions from git lab However becasue is a card game and who gets the car set marely depends on pure luck each pull of cards or attack in this case will depend on how lucky u are to get the most powerful card. for this specifc inttend of game. i decided to live the health untocuh so both enemy and you start at the same level.
---
+++ assets/game.js
@@ -0,0 +1,147 @@
+
+// document.getElementById("yugi").style.backgroundImage.= "url('https://www.gamereactor.eu/media/83/veckansxboxlive_228315b.jpg')";
+$("body").css("background-image", "url('images/stadium.jpg')", "height:900px", "width: 900px");
+
+var opponentPower = Math.floor( ( Math.random() * 1000 ) + 1 );
+var powerAttack = Math.floor( ( Math.random() * 1000 ) + 1 );
+var live = 5;
+var enemy = 5;
+var powerUps= '';///ask for power ups and randomly chosse a image.
+var images = ["images/girl.jpg", "images/yugi.jpg", "images/seto.png","images/sultan.png","assets/images/abidos.png"];
+var cardImages = ["images/card1.png","images/cards2.png"];
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", "assets/sounds/loose.wav");
+var audioElement2 = document.createElement("audio");
+audioElement2.setAttribute("src", "assets/sounds/clapping.wav");
+
+
+ $(document).ready(function() {
+
+	 $('#btn1').hide();
+	 $('#counter').hide();
+	 $('#btn2').hide();
+
+
+
+
+
+$('#tr').on('click', function(){
+randomImage = images[Math.floor(Math.random() * images.length)];
+$("#images-play").html("<img src=" + randomImage + " width='150px'; height='150px'>");
+randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+$("#cards").html("<img src=" + randomCard +" width='300px'; height='200px'>");
+randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+$("#cardo").html("<img src=" + randomCard +" width='300px'; height='150px'>");
+$("#YouFight").html("<h3> Your opponent is</h3>");
+$("#begin").html("<h4> click Attack to begin the tournament!!</h4>");
+$('#er').hide();
+$('#players').hide();
+$('#fr').hide();
+$('#btn2').show();
+
+
+
+});
+//payers her
+$('#players').on('click', function(){
+randomImage = images[Math.floor(Math.random() * images.length)];
+$("#images-play").html("<img src=" + randomImage +" width='150px'; height='150px'>");
+randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+$("#cards").html("<img src=" + randomCard +" width='300px'; height='200px'>");
+randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+$("#cardo").html("<img src=" + randomCard +" width='300px'; height='150px'>");
+$("#YouFight").html("<h3> Your opponent is</h3>");
+$("#begin").html("<h4> click Attack to begin the tournament!!</h4>");
+$('#er').hide();
+$('#tr').hide();
+$('#fr').hide();
+$('#btn2').show();
+
+
+
+});
+
+$('#er').on('click', function(){
+randomImage = images[Math.floor(Math.random() * images.length)];
+$("#images-play").html("<img src=" + randomImage + " width='150px'; height='150px'>");
+randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+$("#cards").html("<img src=" + randomCard +" width='300px'; height='200px'>");
+randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+$("#cardo").html("<img src=" + randomCard +" width='300px'; height='150px'>");
+$("#YouFight").html("<h3> Your opponent is</h3>");
+$("#begin").html("<h4> click Attack to beging the tournament!!</h4>");
+$('#tr').hide();
+$('#players').hide();
+$('#fr').hide();
+$('#btn2').show();
+
+
+
+});
+
+$('#fr').on('click', function(){
+randomImage = images[Math.floor(Math.random() * images.length)];
+$("#images-play").html("<img src=" + randomImage + " width='150px'; height='150px'>");
+randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+$("#cards").html("<img src=" + randomCard +" width='300px'; height='200px'>");
+randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+$("#cardo").html("<img src=" + randomCard +" width='300px'; height='150px'>");
+$("#YouFight").html("<h3> Your opponent is</h3>");
+$("#begin").html("<h4> click Attack to beging the tournament!!</h4>");
+$('#er').hide();
+$('#players').hide();
+$('#tr').hide();
+$('#btn2').show();
+
+
+
+});
+
+
+
+
+
+// var computerGuess = opponentPower[Math.floor(Math.random() * opponentPower.length)];
+
+$('#btn1').click(function the(event){
+document.location.reload();
+
+});
+$('#btn2').click(function the(event){
+$('#counter').show();
+enemyCardplay = opponentPower = Math.floor( ( Math.random() * 1000 ) + 1 );
+yourCounterattacks = powerAttack = Math.floor( ( Math.random() * 1000 ) + 1 );
+if ((powerAttack) > (opponentPower) ){
+enemy--;
+}else{live--;}
+if(live < 0){
+$('#btn1').show();
+ //alert("you lost");
+ audioElement.play();
+ alert("Losser Click the button to play again");
+ $(".container").html("<img src='images/gameover.jpg';  width='500px'; height='400px'>");
+
+
+//document.location.reload();
+}if (enemy < 0) {
+audioElement2.play();
+ alert("winner!!!! Click the button to play again");
+ $(".container").html("<img src='images/winer.jpg';  width='500px'; height='400px'>");
+ $('#btn1').show();
+}  
+
+console.log(live);
+
+$('#counter').html('<p>your Health: ' + live + '</p>' +
+'<p>Enemy cards power: ' + enemyCardplay + '</p>' + '<p>Your Card power: ' + yourCounterattacks + '</p>' + '<p>Enemy Health: ' + enemy + '</p>')
+console.log(this);
+});
+
+ 
+
+
+
+
+});
+
+
